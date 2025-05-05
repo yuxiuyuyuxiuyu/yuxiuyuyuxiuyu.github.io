9221b2fd58a196443cd5860a0fa5c694e7530f29
于修余 <2441542303@qq.com>
2025-05-05 17:01:09 +0800

    Site updated: 2025-05-05 17:01:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/05/文章名/","2025/05/05/hello-world/"];function toRandomPost(){
+var posts=["2025/05/05/hello-world/","2025/05/05/文章名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
